141797144e9d89d3e8fa7927aba5e07ccf0b9311
jahhh-kim <jaes9930@gmail.com>
2020-09-21 13:35:10 +0900

    클래스 라이브러리 프로젝트 생성 .Net Standard 기반 프로젝트 추가 및 모델 클래스 생성

---
+++ MachineApp.Models/Machine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MachineApp.Models
+{
+    /// <summary>
+    /// [2]Model Class : Machine 모델 클래스 == Machines 테이블과 일대일로 매핑 Machie , MachineModel , MachineBase 
+    /// </summary>
+    class Machine
+    {
+        /// <summary>
+        /// Serial Numder
+        /// </summary>
+        public int id { get; set; }
+
+        /// <summary>
+        /// HardWare Name
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public string Created { get; set; }
+
+        public string ModifiedBy { get; set; }
+
+        public DateTime? Modified { get; set; }
+
+    }
+}
--- MachineApp.SqlServer/dbo/Tables/Machines.sql
+++ MachineApp.SqlServer/dbo/Tables/Machines.sql
@@ -3,4 +3,9 @@
 	[Id] INT NOT NULL Identity(1,1) PRIMARY KEY, --Serial Number
 	[Name] NVarChar(255) Not Null,               --H/W Name
 
+	[CreatedBy] NVarChar(255) Null,
+	[Created] DateTime Default (GetDate()),
+	[ModifieBy]NVarChar(255) Null,
+	[Modified] DateTime Null,
+
 )
