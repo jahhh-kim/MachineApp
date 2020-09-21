8f1d57a3523d53605ae658f282c038e0f9b91198
jahhh-kim <jaes9930@gmail.com>
2020-09-21 14:14:51 +0900

    Repository interface 생성 및 비동기 메서드 이름 설계

--- MachineApp.Models/Machine.cs
+++ MachineApp.Models/Machine.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 
 namespace MachineApp.Models
 {
     /// <summary>
     /// [2]Model Class : Machine 모델 클래스 == Machines 테이블과 일대일로 매핑 Machie , MachineModel , MachineBase 
     /// </summary>
-    class Machine
+    public class Machine
     {
         /// <summary>
         /// Serial Numder
@@ -26,6 +28,61 @@ namespace MachineApp.Models
         public string ModifiedBy { get; set; }
 
         public DateTime? Modified { get; set; }
+
+    }
+    /// <summary>
+    /// [3] Repository Interface
+    /// Add Get Update Remove
+    /// </summary>
+    public interface IMachineRepository
+    {
+        Task<Machine> AddMachineAsync(); //입력
+        Task<List<Machine>> GetMachinesAsync(); //출력
+        Task<Machine> GetMachineByIdAsync(); //상세 GetById(), FindById()
+        Task<Machine> EditMachineAsync(Machine machine); //수정
+        Task DeleteMachine(int id); //삭제 
+    }
+
+    /// <summary>
+    /// [4] Repository Class
+    /// </summary>
+    public class MachineRepository : IMachineRepository
+    {
+        public Task<Machine> AddMachineAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Machine>> GetMachinesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Machine> GetMachineByIdAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Machine> EditMachineAsync(Machine machine)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMachine(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+      
+      
+
+     
+    }
+    /// <summary>
+    /// [5] DcContext Class
+    /// </summary>
+    public class MachineDbContext
+    {
 
     }
 }
